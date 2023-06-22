397b66eece8bb97d6d186d9d05e3ae6514e8b534
Aeterrno丶坚果 <11604966+ewallnut2022@user.noreply.gitee.com>
2023-06-22 18:52:50 +0800

    interaction fin. AI still currently disabled

---
+++ src/pages/Server/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const server = require('http').createServer(app);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+    cors: {
+     
+      methods: ['GET', 'POST']
+    }
+  });
+app.use(cors({
+  origin: ['http://localhost:8080','http://localhost:8081'], // 允许的源地址
+  methods: ['GET', 'POST'], // 允许的请求方法
+}));
+
+// 存储连接的用户信息
+const connectedUsers = {};
+
+// 监听连接事件
+io.on('connection', socket => {
+  // 监听前端发送的消息事件
+  socket.on('sendMessage', ({ sender, content }) => {
+    // 将消息发送给其他用户
+    socket.broadcast.emit('receiveMessage', { sender, content });
+  });
+  socket.on('receiveMessage', ({ sender, content }) => {
+    // 处理接收到的消息
+    console.log('Received content:', sender, content);
+    // 这里可以编写进一步处理接收消息的逻辑
+  });
+});
+
+app.use(express.json()); // 添加 JSON 解析中间件
+
+app.post('/msg', (req, res) => {
+  const { sender, content } = req.body;
+  // 群发消息给其他连接的用户
+  io.emit('receiveMessage', { sender, content });
+  console.log(sender);
+  console.log(content);
+  res.sendStatus(200); // 返回成功状态码
+});
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+http.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
